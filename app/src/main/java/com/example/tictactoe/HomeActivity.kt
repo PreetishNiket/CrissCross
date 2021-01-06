f6689cb9caa7e03c5cfe6cd10a09aacd476a6c77
Preetish Niket <niketpreetish@gmail.com>
2021-01-06 17:35:39 +0530

    Completed App with all necessary features

--- app/src/main/java/com/example/tictactoe/HomeActivity.kt
+++ app/src/main/java/com/example/tictactoe/HomeActivity.kt
@@ -23,81 +23,82 @@ import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.rate_custom_layout.view.*
 
 
-class HomeActivity : AppCompatActivity(){
+class HomeActivity : AppCompatActivity() {
     private lateinit var mediaPlayer: MediaPlayer
-    private  var rat=0F
+    private var rat = 0F
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         supportActionBar?.hide()
-        //val animDeep = AnimationUtils.loadAnimation(applicationContext, R.anim.deep)
-        mediaPlayer= MediaPlayer.create(this,R.raw.button_pop)
-        mediaPlayer.setVolume(10F,10F)
-        mediaPlayer.isLooping=false
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.button_pop)
+        mediaPlayer.setVolume(10F, 10F)
+        mediaPlayer.isLooping = false
 
         multi.setOnClickListener {
-            //multi.startAnimation(animDeep)
-            startActivity(Intent(this,MainActivity::class.java))
+
+            startActivity(Intent(this, MainActivity::class.java))
             mediaPlayer.start()
-            overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
         }
 
         vsAndroid.setOnClickListener {
-           // vsAndroid.startAnimation(animDeep)
+
             mediaPlayer.start()
-            startActivity(Intent(this,AndroidActivity::class.java))
-            //overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left)
+            startActivity(Intent(this, AndroidActivity::class.java))
+
         }
         rate_us_btn.setOnClickListener {
-            //rate_us_btn.startAnimation(animDeep)
-//            val number="+919599478721"
-//            val num= String.format("smsto:",number)
-
-            Toast.makeText(this,"Please Grant Sms Permission Manually in App info,If you want your feedback to reach me",Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                this,
+                "Please Grant Sms Permission Manually in App info,if the app do not ask,If you want your feedback to reach me",
+                Toast.LENGTH_LONG
+            ).show()
             mediaPlayer.start()
-            val dialog= AlertDialog.Builder(this)
-            val dialogView= LayoutInflater.from(this).inflate(R.layout.rate_custom_layout,null)
+            val dialog = AlertDialog.Builder(this)
+            val dialogView = LayoutInflater.from(this).inflate(R.layout.rate_custom_layout, null)
             dialog.setView(dialogView)
-            val alertDialog=dialog.show()
-            dialogView.ratingBar.rating=load()
+            val alertDialog = dialog.show()
+            dialogView.ratingBar.rating = load()
             dialogView.ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
-                rat=rating
+                rat = rating
                 save(rat)
                 alertDialog.dismiss()
 
-                val perms= arrayOf(Manifest.permission.SEND_SMS)
-                if (ContextCompat.checkSelfPermission(baseContext,
-                        Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED)
-                {
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                            Manifest.permission.SEND_SMS))
-                    {
-                        ActivityCompat.requestPermissions(this,
-                            perms, 1);
-                    }
-                    else
-                    {
-                        ActivityCompat.requestPermissions(this,
-                            perms, 1);
+                val perms = arrayOf(Manifest.permission.SEND_SMS)
+                if (ContextCompat.checkSelfPermission(
+                        baseContext,
+                        Manifest.permission.SEND_SMS
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(
+                            this,
+                            Manifest.permission.SEND_SMS
+                        )
+                    ) {
+                        ActivityCompat.requestPermissions(
+                            this,
+                            perms, 1
+                        );
+                    } else {
+                        ActivityCompat.requestPermissions(
+                            this,
+                            perms, 1
+                        );
                     }
 
-                }
-                else{
-                    val smsManager=SmsManager.getDefault()
-                    smsManager.sendTextMessage("+919599478721",null,"Rating:$rat",null,null)
+                } else {
+                    val smsManager = SmsManager.getDefault()
+                    smsManager.sendTextMessage("+919599478721", null, "Rating:$rat", null, null)
                 }
 
             }
             alertDialog.show()
-
-
-           // startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$packageManager")))
-
         }
         owner_info.setOnClickListener {
-            //owner_info.startAnimation(animDeep)
             mediaPlayer.start()
-            startActivity(Intent(this,OwnerActivity::class.java))
+            startActivity(Intent(this, OwnerActivity::class.java))
         }
     }
 
@@ -106,38 +107,44 @@ class HomeActivity : AppCompatActivity(){
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        when(requestCode)
-        {
-            1->{
+        when (requestCode) {
+            1 -> {
                 if (grantResults.isNotEmpty() &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
-                    if (ContextCompat.checkSelfPermission(this,
-                            Manifest.permission.SEND_SMS) ==  PackageManager.PERMISSION_GRANTED)
-                    {
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.SEND_SMS
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
                         //Toast.makeText(this, "Permission granted,Now Rate us", Toast.LENGTH_SHORT).show();
-                        val smsManager=SmsManager.getDefault()
-                        smsManager.sendTextMessage("+919599478721",null,"Rating:${rat}",null,null)
+                        val smsManager = SmsManager.getDefault()
+                        smsManager.sendTextMessage(
+                            "+919599478721",
+                            null,
+                            "Rating:${rat}",
+                            null,
+                            null
+                        )
                     }
-                }
-                else
-                {
+                } else {
                     Toast.makeText(this, "No Permission granted", Toast.LENGTH_SHORT).show();
                 }
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
-    private fun save(f:Float)
-    {
-        val pref=getSharedPreferences("save", Context.MODE_PRIVATE)
-        val editor=pref.edit()
-        editor.putFloat("rating",f)
+
+    private fun save(f: Float) {
+        val pref = getSharedPreferences("save", Context.MODE_PRIVATE)
+        val editor = pref.edit()
+        editor.putFloat("rating", f)
         editor.apply()
     }
+
     private fun load(): Float {
-        val sharedPref=getSharedPreferences("save", Context.MODE_PRIVATE)
-        val f=sharedPref.getFloat("rating",0F)
+        val sharedPref = getSharedPreferences("save", Context.MODE_PRIVATE)
+        val f = sharedPref.getFloat("rating", 0F)
         return f
     }
 
@@ -150,7 +157,12 @@ class HomeActivity : AppCompatActivity(){
 
         this.doubleBackToExitPressedOnce = true
         // Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
-        StyleableToast.makeText(this,"Please click BACK again to exit", Toast.LENGTH_SHORT,R.style.exit).show()
+        StyleableToast.makeText(
+            this,
+            "Please click BACK again to exit",
+            Toast.LENGTH_SHORT,
+            R.style.exit
+        ).show()
     }
 
     override fun onResume() {
